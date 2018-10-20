554040e3cc9d9d4390cd96c4121acc66a4b18a50
Christian Schweigel <schwchrisd@web.de>
2018-10-20 17:34:22 +0200

    Added splash screen

---
+++ res/Splash Screen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready():
+	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	# Let two frames pass to make sure the screen was captured
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+
+	# Retrieve the captured image
+	var img = get_viewport().get_texture().get_data()
+  
+	# Flip it on the y-axis (because it's flipped)
+	img.flip_y()
+	img.save_png("splash_screen.png")

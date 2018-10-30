58b473d8c62a105c36f25e8acb35b07973f4c7f0
Christian Schweigel <schwchrisd@web.de>
2018-10-30 17:59:33 +0100

    Fixed crash when using the arrow keys

--- Objects/KeyboardController2.gd
+++ Objects/KeyboardController2.gd
@@ -4,10 +4,12 @@ var Player
 
 func _ready():
 	set_process(false)
+	set_physics_process(false)
 	
 func set_player(player):
 	Player = player
 	set_process(true)
+	set_physics_process(true)
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up_sec"):
